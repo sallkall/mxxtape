a3400d68814511708f2356733beeaca1f671fbee
Sally Zi Kang <sallyzi.kang@mail.utoronto.ca>
2020-02-26 14:44:43 -0500

    updated new post popup, still have to do onsubmit handling

--- mxxtape/src/react-components/Community/NewPost/index.js
+++ mxxtape/src/react-components/Community/NewPost/index.js
@@ -2,41 +2,92 @@ import React from "react";
 import "./styles.css";
 import 'antd/dist/antd.css';
 
-import { Button, Modal, Form, Input, Radio, Icon } from 'antd';
+import {Button, Modal, Form, Input, Icon, Upload, Rate, Layout} from 'antd';
 
-const CollectionCreateForm = Form.create({ name: 'form_in_modal' })(
+const {Sider, Content} = Layout;
+
+const {TextArea} = Input;
+
+const CollectionCreateForm = Form.create({name: 'form_in_modal'})(
     // eslint-disable-next-line
     class extends React.Component {
+        normFile = e => {
+            console.log('Upload event:', e);
+            if (Array.isArray(e)) {
+                return e;
+            }
+            return e && e.fileList;
+        };
+
         render() {
-            const { visible, onCancel, onCreate, form } = this.props;
-            const { getFieldDecorator } = form;
+            const {visible, onCancel, onCreate, form} = this.props;
+            const {getFieldDecorator} = form;
             return (
                 <Modal
                     visible={visible}
-                    title="New Post"
-                    okText="Create"
+                    title="Create Post"
+                    okText="POST"
+                    cancelText="CANCEL"
                     onCancel={onCancel}
                     onOk={onCreate}
+                    width={750}
                 >
-                    <Form layout="vertical">
-                        <Form.Item label="Title">
-                            {getFieldDecorator('title', {
-                                rules: [{ required: true, message: 'Please input the title of your post!' }],
-                            })(<Input />)}
-                        </Form.Item>
-                        <Form.Item label="Description">
-                            {getFieldDecorator('description')(<Input type="textarea" />)}
-                        </Form.Item>
-                        <Form.Item className="collection-create-form_last-form-item">
-                            {getFieldDecorator('modifier', {
-                                initialValue: 'public',
-                            })(
-                                <Radio.Group>
-                                    <Radio value="public">Public</Radio>
-                                    <Radio value="private">Private</Radio>
-                                </Radio.Group>,
-                            )}
-                        </Form.Item>
+                    <Form layout="horizontal">
+                        <Layout className="form_layout">
+                            <Content className="music_display" >
+                                <div><h1>Track: Untitled</h1></div>
+                                <Form.Item label="">
+                                    {getFieldDecorator('dragger', {
+                                        valuePropName: 'fileList',
+                                        getValueFromEvent: this.normFile,
+                                    })(
+                                        <Upload.Dragger name="files" action="/upload.do">
+                                            <p className="ant-upload-drag-icon">
+                                                <Icon type="inbox"/>
+                                            </p>
+                                            <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                                            <p className="ant-upload-hint">Support for a single or bulk upload.</p>
+                                        </Upload.Dragger>,
+                                    )}
+                                </Form.Item>
+                            </Content>
+                            <Sider className="post_sider" width={400}>
+                                <Form.Item
+                                    label=""
+                                    help="Character remaining: 140"
+                                >
+                                    {getFieldDecorator('post_content', {
+                                        // initialValue: 'public',
+                                        // getFieldValue:
+                                    })(
+                                        <TextArea rows={4} placeholder="What's on your mind?"/>
+                                    )}
+                                    {/*{getFieldDecorator('post_content', {*/}
+                                    {/*    // rules: [{ required: false, message: 'Please input the title of your post!' }],*/}
+                                    {/*})(<TextArea rows={4} placeholder="What's on your mind?" />)}*/}
+                                    <div>
+                                        <Rate className="ratings" character={<Icon type="thunderbolt" theme="filled"/>}
+                                              allowHalf/>
+                                        <br/>
+                                    </div>
+                                </Form.Item>
+                                {/*<Form.Item label="Description">*/}
+                                {/*    {getFieldDecorator('description', {*/}
+                                {/*        rules: [{ required: true, message: 'Please input the title of your post!' }]*/}
+                                {/*    })(<Input type="textarea" />)}*/}
+                                {/*</Form.Item>*/}
+                                {/*<Form.Item className="collection-create-form_last-form-item">*/}
+                                {/*    {getFieldDecorator('modifier', {*/}
+                                {/*        initialValue: 'public',*/}
+                                {/*    })(*/}
+                                {/*        <Radio.Group>*/}
+                                {/*            <Radio value="public">Public</Radio>*/}
+                                {/*            <Radio value="private">Private</Radio>*/}
+                                {/*        </Radio.Group>,*/}
+                                {/*    )}*/}
+                                {/*</Form.Item>*/}
+                            </Sider>
+                        </Layout>
                     </Form>
                 </Modal>
             );
@@ -50,15 +101,15 @@ class NewPost extends React.Component {
     };
 
     showModal = () => {
-        this.setState({ visible: true });
+        this.setState({visible: true});
     };
 
     handleCancel = () => {
-        this.setState({ visible: false });
+        this.setState({visible: false});
     };
 
     handleCreate = () => {
-        const { form } = this.formRef.props;
+        const {form} = this.formRef.props;
         form.validateFields((err, values) => {
             if (err) {
                 return;
@@ -66,7 +117,7 @@ class NewPost extends React.Component {
 
             console.log('Received values of form: ', values);
             form.resetFields();
-            this.setState({ visible: false });
+            this.setState({visible: false});
         });
     };
 
@@ -78,7 +129,7 @@ class NewPost extends React.Component {
         return (
             <div>
                 <Button type="primary" onClick={this.showModal}>
-                    New Post <Icon type="form" />
+                    Create Post <Icon type="form"/>
                 </Button>
                 <CollectionCreateForm
                     wrappedComponentRef={this.saveFormRef}
