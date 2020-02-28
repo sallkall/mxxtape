58c39c375e54b903b8558d0b11d6fbeadd1bcf19
Janet Wang <jienet.wang@mail.utoronto.ca>
2020-02-28 14:05:36 -0500

    removed inline styles

--- mxxtape/src/react-components/LoginPage/LoginForm/LoginForm.js
+++ mxxtape/src/react-components/LoginPage/LoginForm/LoginForm.js
@@ -61,7 +61,7 @@ class LoginForm extends React.Component {
                             rules: [{required: true, message: "Please input your username!"}]
                         })(
                             <Input
-                                prefix={<Icon type="user" style={{color: "rgba(0,0,0,.25)"}}/>}
+                                prefix={<Icon type="user" className="input-icon"/>}
                                 placeholder="Username"
                             />
                         )}
@@ -71,7 +71,7 @@ class LoginForm extends React.Component {
                             rules: [{required: true, message: "Please input your password!"}]
                         })(
                             <Input
-                                prefix={<Icon type="lock" style={{color: "rgba(0,0,0,.25)"}}/>}
+                                prefix={<Icon type="lock" className="input-icon"/>}
                                 type="password"
                                 placeholder="Password"
                             />
