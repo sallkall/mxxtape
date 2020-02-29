a93e2ad3111a3cba93d74c74a3f3d17bb93a91f0
Janet Wang <jienet.wang@mail.utoronto.ca>
2020-02-29 00:36:33 -0500

    finished user settings layout

--- mxxtape/src/react-components/SettingsPage/UserSettingsForm/index.js
+++ mxxtape/src/react-components/SettingsPage/UserSettingsForm/index.js
@@ -1,15 +1,102 @@
 import React from "react";
+import './index.css'
+import {Icon, List, Button} from 'antd'
 
 class UserSettingsForm extends React.Component{
     constructor(props) {
         super(props);
+        this.state = {
+            username: this.props
+        };
+    }
+
+    //need server call to get user information
+    componentDidMount() {
+        //make server call with state.username
+        //populate
+        this.setState(
+            {
+                email: "user@user.com",
+                displayName: "user-display-name",
+                about: "Lorem Ipsum...?",
+                spotifyConnected: "user-spotify-account",
+                avatar: "https://yt3.ggpht.com/a/AGF-l7-11--_67EpTJhLCO6c4xBXPLHhC0C4GXaoQg=s900-c-k-c0xffffffff-no-rj-mo"
+            }
+        );
     }
 
     render() {
         return (
             <div>
-                <h2>Account Settings</h2>
+                <header className="settings-header">
+                    <Icon type="AiFillSetting" className="header-icon"/>
+                    Account Settings
+                </header>
+
+                <header className="settings-subheader">ACCOUNT PREFERENCES</header>
                 <hr/>
+                <List>
+                    <List.Item>
+                        <List.Item.Meta
+                            title={"Email Address"}
+                            description={this.state.email}
+                        />
+                        <Button type="button">CHANGE</Button>
+                    </List.Item>
+                    <List.Item>
+                        <List.Item.Meta
+                            title={"Change Password"}
+                            description={this.state.email}
+                        />
+                        <Button type="button">CHANGE</Button>
+                    </List.Item>
+                    <List.Item>
+                        <List.Item.Meta
+                            title={"Spotify Account"}
+                            description={ this.state.spotifyConnected ? this.state.spotifyConnected : "Not Connected :("}
+                        />
+                        <Button type="button">Change Spotify account</Button>
+                    </List.Item>
+                    <List.Item>
+                        <List.Item.Meta
+                            title={"Deactivate Account"}
+                        />
+                        <Button type="button" className="red"><Icon type="delete"/>Deactivate Account</Button>
+                    </List.Item>
+                </List>
+                <header className="settings-header">
+                    <Icon type="AiFillSetting" className="header-icon"/>
+                    Customize profile
+                </header>
+                <header className="settings-subheader">PROFILE INFORMATION</header>
+                <hr/>
+                <List>
+                    <List.Item>
+                        <List.Item.Meta
+                            title={"Display Name"}
+                            description={this.state.displayName}
+                        />
+                        <Button type="button">Change display name</Button>
+                    </List.Item>
+                    <List.Item>
+                        <List.Item.Meta
+                            title={"About"}
+                            description={this.state.about}
+                        />
+                        <Button type="button">Change about</Button>
+                    </List.Item>
+                </List>
+                <header className="settings-subheader">Images</header>
+                <hr/>
+                <List>
+                    <List.Item>
+                        <List.Item.Meta
+                            title={"Avatar"}
+                            description={<img className="settings-avatar" src={this.state.avatar} /> }
+                        />
+                        <Button type="button">CHANGE</Button>
+                    </List.Item>
+                </List>
 
             </div>
         )
--- mxxtape/src/react-components/SettingsPage/index.js
+++ mxxtape/src/react-components/SettingsPage/index.js
@@ -9,17 +9,26 @@ class SettingsPage extends React.Component {
     render() {
         const { state } = this.props;
 
+        //will need api call to find out if the user is an admin or regular user
         if (state.loggedIn === -1) {
             return (<Redirect to='/login'/>)
         } else if (state.loggedIn === 1) {
-            // const UserSettingsForm
             return (
                 <div>
                     <Nav state={ state }/>
                     <div className="settingsForm" >
                         <UserSettingsForm state={state}/>
                     </div>
-
+                </div>
+            )
+        } else if (state.loggedIn == 2) {
+            return (
+                <div>
+                    <Nav state={ state }/>
+                    <div className="settingsForm" >
+                        Welcome admin.
+                        <UserSettingsForm state={state}/>
+                    </div>
                 </div>
             )
         }
