869d5063e945dffd7b68cc556e86b61af2eb0e34
Janet Wang <jienet.wang@mail.utoronto.ca>
2020-03-01 18:16:11 -0500

    lock icons added

--- mxxtape/src/react-components/ForgotPasswordPage/ForgotPasswordForm/index.js
+++ mxxtape/src/react-components/ForgotPasswordPage/ForgotPasswordForm/index.js
@@ -2,10 +2,26 @@ import React from "react";
 
 import "./index.css";
 import 'antd/dist/antd.css';
-import {Button, Form, Icon, Input} from "antd";
+import {Button, Form, Icon, Input, message} from "antd";
 import {withRouter} from "react-router-dom";
 
 class ForgotPasswordForm extends React.Component {
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log("Received values of form: ", values);
+                // if (checkValidEmail(values.email)) {
+                    // will eventually need to make server calls to make new users
+                    // for now nothing happens
+                    message.success('Password Change Successful! Please login to continue');
+                    this.props.history.push("login");
+                // }
+            }
+        });
+    };
+
     render() {
         const {getFieldDecorator} = this.props.form;
         return (
@@ -34,7 +50,10 @@ class ForgotPasswordForm extends React.Component {
                     ]}
                     hasFeedback
                 >
-                    <Input.Password className="input" placeholder="New password"/>
+                    <Input.Password
+                        className="input"
+                        prefix={<Icon type="lock" className="input-icon"/>}
+                        placeholder="New password"/>
                 </Form.Item>
 
                 <Form.Item
@@ -56,7 +75,10 @@ class ForgotPasswordForm extends React.Component {
                         }),
                     ]}
                 >
-                    <Input.Password className="input" placeholder="Confirm password"/>
+                    <Input.Password
+                        className="input"
+                        prefix={<Icon type="lock" className="input-icon"/>}
+                        placeholder="Confirm password"/>
                 </Form.Item>
                 <Form.Item>
                     <Button
