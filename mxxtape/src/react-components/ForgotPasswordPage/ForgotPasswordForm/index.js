9cadfe3239ad0d06c78e7ec22f5ec4a95f51dca1
Janet Wang <jienet.wang@mail.utoronto.ca>
2020-03-01 17:05:53 -0500

    added password form

--- mxxtape/src/react-components/ForgotPasswordPage/ForgotPasswordForm/index.js
+++ mxxtape/src/react-components/ForgotPasswordPage/ForgotPasswordForm/index.js
@@ -2,14 +2,71 @@ import React from "react";
 
 import "./index.css";
 import 'antd/dist/antd.css';
-import {Form} from "antd";
+import {Button, Form, Icon, Input} from "antd";
 import {withRouter} from "react-router-dom";
 
 class ForgotPasswordForm extends React.Component {
     render() {
-        const {state} = this.props;
+        const {getFieldDecorator} = this.props.form;
         return (
-            <Form>Forgot Password</Form>
+            <Form
+                onSubmit={this.handleSubmit}
+                className="forgot-password-form"
+            >
+                <Form.Item>
+                    {getFieldDecorator("username", {
+                        rules: [{required: true, message: "Please input your username!"}]
+                    })(
+                        <Input
+                            prefix={<Icon type="user"  className="input-icon"/>}
+                            placeholder="Username"
+                        />
+                    )}
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                    ]}
+                    hasFeedback
+                >
+                    <Input.Password placeholder="New password"/>
+                </Form.Item>
+
+                <Form.Item
+                    name="confirm"
+                    dependencies={['password']}
+                    hasFeedback
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please confirm your password!',
+                        },
+                        ({ getFieldValue }) => ({
+                            validator(rule, value) {
+                                if (!value || getFieldValue('password') === value) {
+                                    return Promise.resolve();
+                                }
+                                return Promise.reject('The passwords that you entered do not match!');
+                            },
+                        }),
+                    ]}
+                >
+                    <Input.Password placeholder="Confirm password"/>
+                </Form.Item>
+                <Form.Item>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        className="button"
+                    >
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
         )
     }
 }
