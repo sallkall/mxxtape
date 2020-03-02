990769bbec6cf0650346b5c5256b7d35c03f4993
Janet Wang <jienet.wang@mail.utoronto.ca>
2020-03-01 22:30:20 -0500

    check for matching passwords OK

--- mxxtape/src/react-components/ForgotPasswordPage/ForgotPasswordForm/index.js
+++ mxxtape/src/react-components/ForgotPasswordPage/ForgotPasswordForm/index.js
@@ -2,7 +2,13 @@ import React from "react";
 
 import "./index.css";
 import 'antd/dist/antd.css';
-import {Button, Form, Icon, Input, message} from "antd";
+import {
+    Form,
+    Input,
+    Button,
+    message,
+    Icon
+} from 'antd';
 import {withRouter} from "react-router-dom";
 
 class ForgotPasswordForm extends React.Component {
@@ -12,29 +18,60 @@ class ForgotPasswordForm extends React.Component {
         console.log("Construct Forgot Password Form", props);
     }
 
+    state = {
+        confirmDirty: false,
+    };
+
     handleSubmit = e => {
         e.preventDefault();
-        this.props.form.validateFields((err, values) => {
+        this.props.form.validateFieldsAndScroll((err, values) => {
             if (!err) {
-                console.log("Received values of form: ", values);
-                // if (checkValidEmail(values.email)) {
-                    // will eventually need to make server calls to make new users
-                    // for now nothing happens
-                    message.success('Password Change Successful! Please login to continue');
-                    this.props.history.push("login");
-                // }
+                console.log('Received values of form: ', values);
+                // will eventually need to make server calls to make change the user password
+                // will need to have callback that the user exists
+                console.log(values.user);
+                // on success send password data to server
+                console.log('changing password', values.password);
+                // for now nothing happens
+                message.success('Password Change Successful! Please login to continue');
+                this.props.history.push('/login');
             }
         });
     };
 
+    handleConfirmBlur = e => {
+        // using sample code from antd
+        // https://3x.ant.design/components/form/
+        const { value } = e.target;
+        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+    };
+
+    compareToFirstPassword = (rule, value, callback) => {
+        // using sample code from antd
+        // https://3x.ant.design/components/form/
+        const { form } = this.props;
+        if (value && value !== form.getFieldValue('password')) {
+            callback('The passwords don\'t match!');
+        } else {
+            callback();
+        }
+    };
+
+    validateToNextPassword = (rule, value, callback) => {
+        // using sample code from antd
+        // https://3x.ant.design/components/form/
+        const { form } = this.props;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(['confirm'], { force: true });
+        }
+        callback();
+    };
+
     render() {
-        const {getFieldDecorator} = this.props.form;
+        const { getFieldDecorator } = this.props.form;
+
         return (
-            <Form
-                onSubmit={this.handleSubmit}
-                className="forgot-password-form"
-                scrollToFirstError
-            >
+            <Form onSubmit={this.handleSubmit} className="forgot-password-form">
                 <Form.Item>
                     {getFieldDecorator("username", {
                         rules: [{required: true, message: "Please input your username!"}]
@@ -92,7 +129,7 @@ class ForgotPasswordForm extends React.Component {
                     </Button>
                 </Form.Item>
             </Form>
-        )
+        );
     }
 }
 export default withRouter(ForgotPasswordForm);
