9ca806b35cc2198b84fe8978e66c875e2b3d34f7
Janet Wang <jienet.wang@mail.utoronto.ca>
2020-03-03 14:36:40 -0500

    reverted to old method, changing approach from dropdown to new page

--- mxxtape/src/react-components/Navigation/index.js
+++ mxxtape/src/react-components/Navigation/index.js
@@ -4,8 +4,8 @@ import {Link, BrowserRouter as Router, withRouter} from "react-router-dom";
 import "./styles.css";
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-import {Menu, Icon, Input, Badge, Dropdown} from 'antd';
-import NotificationMenu from "./NotificationMenu";
+import {Menu, Icon, Input, Badge} from 'antd';
+import NotificationBadge from "./NotificationBadge";
 
 const {SubMenu} = Menu;
 const {Search} = Input;
@@ -14,11 +14,11 @@ class Nav extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state  = {
+            current: 'menu_dashboard',
+            userInfo: {}
+        };
     }
-
-    state = {
-        current: 'menu_dashboard',
-    };
 
     handleSearch(inputValue) {
         let cleanInput = inputValue.replace(/\s/g, "");
@@ -33,71 +33,21 @@ class Nav extends React.Component {
 
     componentDidMount() {
         if (this.state.user) {
-            this.getUserInfo(this.state.user);
+            this.getUserInfo(this.props.state.user);
+            console.log("Nav bar mounted", 5);
         }
     }
 
-    getUserInfo = (user) => {
-        if (user && user !== -1) {
-            // get user's notification list and update state
-            console.log("got unread notifications list from server");
-            const notice = [
-                {
-                    postId: 5,
-                    author: "jazzy cat",
-                    avatar: "https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/s150x150/83885851_189723372356264_5738621742125501341_n.jpg?_nc_ht=scontent-yyz1-1.cd…",
-                    content: "bahahaha",
-                    timestamp: "2020-03-03 01:51:16",
-                    updatedAt:"2020-03-03 01:51:16"
-                },
-                {
-                    read: false,
-                    postId: 4,
-                    author: "Sally Kang",
-                    avatar: 'https://scontent.fyto1-1.fna.fbcdn.net/v/t1.0-9/83337570_3515329088508799_4523582417981669376_n.jpg?_nc_cat=104&_nc_sid=85a577&_nc_ohc=6unpVlo76lgAX_kJl9K&_nc_ht=scontent.fyto1-1.fna&oh=b515f36742c04f5c702bf2426e9f0739&oe=5EF63A44',
-                    content: "Welcome to jazz it up",
-                    timestamp: "2020-03-03 01:51:17",
-                    updatedAt:"2020-03-03 01:51:17"
-                },
-                {
-                    read: false,
-                    postId: 3,
-                    author: "Connor Ferwerda",
-                    avatar: 'https://scontent-yyz1-1.xx.fbcdn.net/v/t1.0-9/17796794_111333216080695_8382139360744649163_n.jpg?_nc_cat=111&_nc_sid=85a577&_nc_ohc=Gf4HcH4bUcQAX_g6YG3&_nc_ht=scontent-yyz1-1.xx&oh=f5c0629f62f78998651c403538c80b3e&oe=5EEE926E',
-                    content: "Lorem...ipsum???",
-                    timestamp: "2020-03-03 01:51:18",
-                    updatedAt:"2020-03-03 01:51:18"
-                },
-                {
-                    read: true,
-                    postId: 2,
-                    author: "Janet Wang",
-                    avatar: 'https://scontent.fyto1-2.fna.fbcdn.net/v/t1.0-9/15871890_1178229362264200_2730942226162743028_n.jpg?_nc_cat=108&_nc_sid=85a577&_nc_ohc=GhhoJ0IXttIAX_Xql-z&_nc_ht=scontent.fyto1-2.fna&oh=65c60f89ab0c02ffb8961f4f69acf7ab&oe=5EFD122F',
-                    content: "something about ui?",
-                    timestamp: "2020-03-03 00:51:18",
-                    updatedAt:"2020-03-03 00:51:18"
-                }
-            ];
-            const userInfo = {
-                name: "jazzy cat",
-                notifyCount: 4,
-                unreadCount: 3,
-                notice: notice
-            };
-
-            this.setState(
-                {user: user},
-                () => console.log(userInfo)
-            );
-
-            return notice;
-        } else {
-            return [];
+    getLoggedInFromStateProp(state) {
+        console.log(state);
+        if (state && state.loggedIn) {
+            return state.loggedIn
         }
-    };
+    }
 
     render() {
         const {state} = this.props;
+        const userInfo = this.state.userInfo ? this.state.userInfo : {};
         return (
             <Router>
                 <Menu
@@ -117,20 +67,14 @@ class Nav extends React.Component {
                                onClick={() => this.redirect('/')}>
                         <Link to="/"><Icon type="compass" theme="twoTone" /> Dashboard </Link>
                     </Menu.Item>
-                    <Menu.SubMenu className='menu-notifications'
+                    <Menu.SubMenu
+                        className='menu-notifications'
                         key="notifications"
+                        onClick={() => {console.log(state.loggedIn)}}
                         title={
-                            <Badge>
-                                <Icon type="bell" theme="twoTone" />
-                            </Badge>
+                            <NotificationBadge user={this.getLoggedInFromStateProp(state)}/>
                         }
-                    >
-                        <Menu.ItemGroup title="Notifications">
-                            <Menu.Item key="setting:1">Notification 1</Menu.Item>
-                            <Menu.Item key="create-community">Notification 2</Menu.Item>
-                        </Menu.ItemGroup>
-                    </Menu.SubMenu>
-
+                    />
                     <SubMenu
                         className='menu_sub'
                         title={
