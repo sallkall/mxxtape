c56f640c1a9aa69cac3eb62e1e525d781d0805ed
Janet Wang <jienet.wang@mail.utoronto.ca>
2020-03-03 02:05:36 -0500

    added get notifications architecture for server call

--- mxxtape/src/react-components/Navigation/NavigationNotification/index.js
+++ mxxtape/src/react-components/Navigation/NavigationNotification/index.js
@@ -12,8 +12,44 @@ class NavigationNotification extends React.Component {
         }
     }
 
+    getNotificationList = (user) => {
+        if (user && user !== -1) {
+            // get user's notification list and update state
+            console.log("got unread notifications list from server");
+            const unread_posts = [
+                {
+                    postId: 5,
+                    author: "jazzy cat",
+                    avatar: "https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/s150x150/83885851_189723372356264_5738621742125501341_n.jpg?_nc_ht=scontent-yyz1-1.cd…",
+                    content: "asdf",
+                    timestamp: "2020-03-03 01:51:16"
+                },
+                {
+                    postId: 4,
+                    author: "jazzy cat",
+                    avatar: "https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/s150x150/83885851_189723372356264_5738621742125501341_n.jpg?_nc_ht=scontent-yyz1-1.cd…",
+                    content: "asdf",
+                    timestamp: "2020-03-03 01:51:16"
+                },
+                {
+                    postId: 3,
+                    author: "jazzy cat",
+                    avatar: "https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/s150x150/83885851_189723372356264_5738621742125501341_n.jpg?_nc_ht=scontent-yyz1-1.cd…",
+                    content: "asdf",
+                    timestamp: "2020-03-03 01:51:16"
+                }
+            ];
+
+            console.log(unread_posts);
+
+            return unread_posts;
+        } else {
+            return [];
+        }
+    };
+
     render() {
-        const notification_list = [1, 2, 3];
+        const notification_list = this.getNotificationList(this.state.user);
 
         return (
             <Badge count={notification_list.length}>
