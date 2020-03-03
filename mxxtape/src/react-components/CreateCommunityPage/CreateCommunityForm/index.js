9cac600ac3e1aadc7b7c86da7fa54554ed987ada
Janet Wang <jienet.wang@mail.utoronto.ca>
2020-03-02 22:49:15 -0500

    layout of create community form done

---
+++ mxxtape/src/react-components/CreateCommunityPage/CreateCommunityForm/index.js
@@ -0,0 +1,92 @@
+import {Form, Input, Tooltip, Icon} from "antd";
+import React from "react";
+import {withRouter} from "react-router-dom";
+import './index.css'
+
+class CreateCommunityForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name_tooltip: "Give your community a good name that doesn't overlap with existing communities! " +
+                "Note that this can't be changed in the future.",
+            genre_tooltip: "List as many genres as you like, but you need at least one.",
+            description_tooltip: "Give an explanation of what you hope to see in your community. This will also be " +
+                "how new members come to understand your community as well as explain what makes your community " +
+                "unique!."
+        }
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if(!err) {
+                console.log("Received values of form: ", values);
+            }
+        });
+    };
+
+    render() {
+        const {getFieldDecorator} = this.props.form;
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Item
+                    label={
+                        <span>
+                            Name your community&nbsp;
+                            <Tooltip title={this.state.name_tooltip} >
+                                <Icon type="question-circle-o" />
+                            </Tooltip>
+                        </span>
+                    }
+                >
+                    {getFieldDecorator("community-name", {
+                        rules: [{required: true, message: "Need a name for a new community!"}]
+                    })(
+                        <Input
+                            className="community-input-short"
+                        />
+                    )}
+                </Form.Item>
+                <Form.Item
+                    label={
+                        <span>
+                            What genres of music will your community be interested in?&nbsp;
+                            <Tooltip title={this.state.genre_tooltip} >
+                                <Icon type="question-circle-o" />
+                            </Tooltip>
+                        </span>
+                    }
+                >
+                    {getFieldDecorator("community-genre", {
+                        rules: [{required: true, message: "Please enter a genre your community will be interested in"}]
+                    })(
+                        <Input
+                            className="community-input"
+                        />
+                    )}
+                </Form.Item>
+                <Form.Item
+                    label={
+                        <span>
+                            Description &nbsp;
+                            <Tooltip title={this.state.description_tooltip} >
+                                <Icon type="question-circle-o" />
+                            </Tooltip>
+                        </span>
+                    }
+                >
+                    {getFieldDecorator("community-description", {
+                        rules: [{required: true, message: "Explain why you think your community is unique."}]
+                    })(
+                        <Input.TextArea
+                            className="community-input"
+                            autoSize={{ minRows: 6, maxRows: 20 }}
+                        />
+                    )}
+                </Form.Item>
+            </Form>
+        )
+    }
+}
+
+export default withRouter(CreateCommunityForm);
--- mxxtape/src/react-components/CreateCommunityPage/index.js
+++ mxxtape/src/react-components/CreateCommunityPage/index.js
@@ -3,12 +3,27 @@ import React from "react";
 import "./styles.css";
 import 'antd/dist/antd.css';
 import Nav from "../Navigation";
+import {Form} from "antd";
+import CreateCommunityForm from "./CreateCommunityForm";
 
 class CreateCommunityPage extends React.Component {
     render() {
         const {state} = this.props;
+        const CreateACommunityForm = Form.create({name: 'create-community'})(
+            CreateCommunityForm
+        );
         return (
-            <div><Nav state={state} /> Create Community</div>
+            <div>
+                <Nav state={state} />
+                <div className="create-community-form">
+
+                    <header className="create-community-header">
+                        Create a community!
+                    </header>
+                    <hr/>
+                    <CreateACommunityForm/>
+                </div>
+            </div>
         )
     }
 }
