1c254afd5e4aca143b2a3c7829764f0c0ae8e0d2
Janet Wang <janet.wang.13@gmail.com>
2020-04-02 17:12:08 -0400

    removed password validator component and added register functionality

--- mxxtape/src/actions/user.js
+++ mxxtape/src/actions/user.js
@@ -1,4 +1,6 @@
 //From example
+import {message} from "antd";
+
 export const readCookie = (app) => {
     console.log("readCookie", app.state);
     const url = "/users/check-session";
@@ -52,6 +54,50 @@ export const login = (loginComp, app) => {
         })
         .catch(error => {
             console.log("login catch error", request, error);
+        });
+};
+
+//register a new user
+export const registerNewUser = (registerComp, history) => {
+    console.log("registerNewUser", registerComp.state);
+    // Create our request constructor with all the parameters we need
+
+    const request = new Request("/users", {
+        method: "post",
+        body: JSON.stringify({
+            email: registerComp.state.email,
+            username: registerComp.state.username,
+            password: registerComp.state.password
+        }),
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+        }
+    });
+
+    // Send the request with fetch()
+    fetch(request)
+        .then(res => {
+            if (res.status === 200) {
+                return res.json();
+            }
+        })
+        .then(json => {
+            if (json) {
+                if (json.username !== undefined){
+                    message.success('Registration Successful! Please login to continue');
+                    history.push('/');
+                } else {
+                    message.error('Something went wrong, try again later');
+                    history.push('/');
+                }
+            } else {
+                message.error('Bad Request! Someone with this email or username already exists!');
+            }
+        })
+        .catch(error => {
+            console.log("register catch error", request, error);
+            message.error('Something went wrong, try again later');
         });
 };
 
--- mxxtape/src/react-components/RegisterPage/RegisterForm/RegisterForm.js
+++ mxxtape/src/react-components/RegisterPage/RegisterForm/RegisterForm.js
@@ -2,11 +2,11 @@ import React from "react";
 import './RegisterForm.css'
 import {Button, Form, Icon, Input, message} from "antd";
 import {withRouter} from "react-router-dom";
-import PasswordValidator from "../../ForgotPasswordPage/PasswordValidator";
+import {registerNewUser} from "../../../actions/user";
 
 function checkValidEmail(email) {
     // regex for email taken from https://emailregex.com
-    const isEmailAddress = email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+    const isEmailAddress = email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
     if (!isEmailAddress){
         message.error('You must enter a valid email address!')
     }
@@ -21,29 +21,69 @@ class RegisterForm extends React.Component {
     }
 
     state = {
-        confirmDirty: false,
+        email: "",
+        username: "",
+        password: "",
+        confirmDirty: false
     };
 
     handleSubmit = e => {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
             if (!err) {
-                console.log("Received values of form: ", values);
-                if (checkValidEmail(values.email)) {
+                console.log("Register handleSubmit: Received values of form: ", values);
+                if (checkValidEmail(values.email) && values.username && values.password) {
                     // will eventually need to make server calls to make new users
                     // for now nothing happens
-                    message.success('Registration Successful! Please login to continue');
-                    this.props.history.push("login");
+                    this.setState(
+                        {
+                            email: values.email,
+                            username: values.username,
+                            password: values.password
+                        },
+                        () => registerNewUser(this, this.props.history)
+                        );
+                } else {
+                    message.error('Inputs invalid! Can\'t register yet');
                 }
             }
         });
     };
 
+    // for handling password validation
+    handleConfirmBlur = e => {
+        // using sample code from antd
+        // https://3x.ant.design/components/form/
+        const { value } = e.target;
+        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+    };
+
+    compareToFirstPassword = (rule, value, callback) => {
+        // using sample code from antd
+        // https://3x.ant.design/components/form/
+        const { form } = this.props;
+        if (value && value !== form.getFieldValue('password')) {
+            callback('The passwords don\'t match!');
+        } else {
+            callback();
+        }
+    };
+
+    validateToNextPassword = (rule, value, callback) => {
+        // using sample code from antd
+        // https://3x.ant.design/components/form/
+        const { form } = this.props;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(['confirm'], { force: true });
+        }
+        callback();
+    };
+
     render() {
         const {getFieldDecorator} = this.props.form;
-        const RegisterPasswordValidator = Form.create({name:'password validator'})(
-            PasswordValidator
-        );
+        // const RegisterPasswordValidator = Form.create({name:'password validator'})(
+        //     PasswordValidator
+        // );
         return (
             <div>
                 <Form
@@ -72,7 +112,42 @@ class RegisterForm extends React.Component {
                             />
                         )}
                     </Form.Item>
-                    <RegisterPasswordValidator handleSubmit={this.handleSubmit}/>
+                    <Form.Item hasFeedback>
+                        {getFieldDecorator('password', {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: 'Please input your password!',
+                                },
+                                {
+                                    validator: this.validateToNextPassword,
+                                },
+                            ],
+                        })(<Input.Password
+                            className="input"
+                            prefix={<Icon type="lock" className="input-icon"/>}
+                            placeholder="New password"/>)}
+                    </Form.Item>
+                    <Form.Item hasFeedback>
+                        {getFieldDecorator('confirm', {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: 'Please confirm your password!',
+                                },
+                                {
+                                    validator: this.compareToFirstPassword,
+                                },
+                            ],
+                        })(
+                            <Input.Password
+                                className="input"
+                                prefix={<Icon type="lock" className="input-icon"/>}
+                                placeholder="Confirm password"
+                                onBlur={this.handleConfirmBlur}
+                            />
+                        )}
+                    </Form.Item>
                     <Form.Item>
                         <Button
                             type="primary"
