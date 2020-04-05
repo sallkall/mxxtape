79c2104e7ab66caa7108d8d5f8fa9037ab67b2e3
Janet Wang <janet.wang.13@gmail.com>
2020-04-04 22:07:07 -0400

    moving file size up to 30

--- mxxtape/src/react-components/SettingsPage/SettingsForm/index.js
+++ mxxtape/src/react-components/SettingsPage/SettingsForm/index.js
@@ -25,9 +25,9 @@ function beforeUpload(file) {
     if (!isJpgOrPng) {
         message.error('You can only upload JPG/PNG file!');
     }
-    const isLt2M = file.size / 1024 / 1024 < 2;
+    const isLt2M = file.size / 1024 < 30;
     if (!isLt2M) {
-        message.error('Image must smaller than 2MB!');
+        message.error('Image must smaller than 30 kb!');
     }
     return isJpgOrPng && isLt2M;
 }
